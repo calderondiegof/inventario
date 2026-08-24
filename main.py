3c6dc2646e314316e8151bbd8ac0046c85484a77
calderondiegof <calderondiegonueva@gmail.com>
2026-08-24 02:02:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1148,5 +1148,5 @@ async def descargar_documento(nombre_archivo: str) -> Response:
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
